ebcb948d4c200392660c97628d5431e434d4e6e1
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2024-06-26 20:50:55 +0300

    Add config template test

---
+++ tests/config.rs
@@ -0,0 +1,12 @@
+#[cfg(test)]
+mod config_tests {
+    use hummingparrot::config::Config;
+    use std::path::Path;
+
+    #[test]
+    fn template() {
+        let config_toml = std::fs::read_to_string(Path::new("config.template.toml"))
+            .expect("read config template file");
+        toml::from_str::<Config>(&config_toml).expect("parse config template file toml");
+    }
+}
