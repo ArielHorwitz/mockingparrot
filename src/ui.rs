23802001194be1324bc3e8b746ad7af04ae403db
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2024-09-09 21:21:30 +0300

    Fix formatting

--- src/ui.rs
+++ src/ui.rs
@@ -316,30 +316,42 @@ fn draw_config(
         .border_style(state.config.ui.colors.frame.normal)
         .title("Note")
         .title_style(state.config.ui.colors.frame.warn);
-    let edit_hotkey_repr = state.config.hotkeys.get(&crate::hotkeys::HotkeyAction::Edit).and_then(|vec_opt| {
-        if let Some(v) = vec_opt {
-            return v.first().map(|h| format!("{:?} + {:?}", h.modifiers, h.code));
-        };
-        None
-    }).unwrap_or_else(|| "<NO HOTKEY SET>".to_string());
-    let note =
-        Text::from_iter([
-            Line::from_iter([
-                Span::styled("It is generally recommend to alter ", state.config.ui.colors.text.normal),
-                Span::styled("top_p", state.config.ui.colors.text.highlight),
-                Span::styled(" or ", state.config.ui.colors.text.normal),
-                Span::styled("temperature", state.config.ui.colors.text.highlight),
-                Span::styled(", but not both.", state.config.ui.colors.text.normal),
-            ])
-            .fg(state.config.ui.colors.text.warn),
-            Line::default(),
-            Line::from_iter([
-                Span::styled("For a all settings, press: ", state.config.ui.colors.text.normal),
-                Span::styled(edit_hotkey_repr, state.config.ui.colors.text.highlight),
-                Span::styled(".", state.config.ui.colors.text.normal),
-            ])
-            .fg(state.config.ui.colors.text.warn),
-        ]);
+    let edit_hotkey_repr = state
+        .config
+        .hotkeys
+        .get(&crate::hotkeys::HotkeyAction::Edit)
+        .and_then(|vec_opt| {
+            if let Some(v) = vec_opt {
+                return v
+                    .first()
+                    .map(|h| format!("{:?} + {:?}", h.modifiers, h.code));
+            };
+            None
+        })
+        .unwrap_or_else(|| "<NO HOTKEY SET>".to_string());
+    let note = Text::from_iter([
+        Line::from_iter([
+            Span::styled(
+                "It is generally recommend to alter ",
+                state.config.ui.colors.text.normal,
+            ),
+            Span::styled("top_p", state.config.ui.colors.text.highlight),
+            Span::styled(" or ", state.config.ui.colors.text.normal),
+            Span::styled("temperature", state.config.ui.colors.text.highlight),
+            Span::styled(", but not both.", state.config.ui.colors.text.normal),
+        ])
+        .fg(state.config.ui.colors.text.warn),
+        Line::default(),
+        Line::from_iter([
+            Span::styled(
+                "For a all settings, press: ",
+                state.config.ui.colors.text.normal,
+            ),
+            Span::styled(edit_hotkey_repr, state.config.ui.colors.text.highlight),
+            Span::styled(".", state.config.ui.colors.text.normal),
+        ])
+        .fg(state.config.ui.colors.text.warn),
+    ]);
     frame.render_widget(note, note_block.inner(*bottom_layout));
     frame.render_widget(note_block, *bottom_layout);
     Ok(())
